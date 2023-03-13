07b53377244c9c15724f880d0e8b66110c3e6999
beautyoda <127671598+beautyoda@users.noreply.github.com>
2023-03-13 10:25:46 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,45 @@
+$(function () {
+	
+    // カレンダー
+    $(function () {
+        $('input[name="date"]').datepicker({
+            dateFormat: 'yy年mm月dd日',
+    // 昨日の日付以降を選択できなくする
+            minDate:0
+        });
+    });
+
+
+    // 予約フォームを表示する
+    $('#form-number').click(function () {
+        $('#form-name').empty();
+        var date = $('input[name="date"]').val();
+        var num = $('input[name="number"]:checked').val();
+        var minute = $('select[name="minute"]').val();
+        var names = $('select[name="names"]').val();
+        var inquiries = $('textarea[name="inquiries"]').val();
+    });
+
+    // 送信
+    $('form').submit(function () {
+        var date = $('input[name="date"]').val();
+        var number = $('input[name="number"]:checked').val();	
+        var minute = $('select[name="minute"]').val();
+        var names = $('select[name="names"]').val();
+        var inquiries = $('textarea[name="inquiries"]').val();
+	    
+        var msg = `＊＊＊ご予約内容＊＊＊\n〓希望日：〓\n ${date}\n〓時間：〓\n ${minute}\n〓メニュー：〓\n ${names}\n〓問い合わせ内容：〓\n ${inquiries}`;
+        sendText(msg);
+
+        return false;
+    });
+	
+    // 送信
+    $('form').submit(function () {
+	var msg = `ご連絡が遅れる場合がございますが、\n確認後に返信致します。`;
+        sendText(msg);
+
+        return false;
+    });
+	
+});
